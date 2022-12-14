1c86df5d4579d20a9862551336696b90d39c7b61
Eghosa Ordia <eghordia130@gmail.com>
2022-12-14 01:10:11 +0100

    Changes

--- multi-step-form-main/app.js
+++ multi-step-form-main/app.js
@@ -1,23 +1,39 @@
+// Input
+const name_input = document.getElementById("name");
+const email_input = document.getElementById("email");
+const number_input = document.getElementById("number");
+
 const active = document.querySelectorAll("active");
-const checkbox = document.querySelector(".checkbox");
 const pricePeriod = document.querySelectorAll(".select-per");
 const slider = document.querySelector(".slider");
 const btn_step = document.querySelector(".btm-btn");
 const step_2 = document.querySelector(".step-2");
 const step_3 = document.querySelector(".step-3");
-
 const num = document.querySelectorAll(".num");
 const steps = document.querySelectorAll(".step");
 const backBtn = document.querySelector(".go-back");
 
+const step_3_div = document.querySelectorAll(".add");
+const checkbox = document.querySelectorAll(".checkbox");
 
-let checker = 1;
+const btn_change = document.querySelector(".btn-chng");
+
+// Validation Function
+// validate = () => {
+//   if (name_input && email_input && number_input) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+// Implementing the Next Step Feature
+let checker = 0;
 checkDiv = () => {
   if (checker > steps.length - 1) {
-    // checker = 0;
     checker = steps.length - 1;
-    //TODO: FINAL PAGE
   }
+  checker++;
+  console.log(checker);
   steps.forEach((e) => {
     e.classList.remove("active");
   });
@@ -26,14 +42,13 @@ checkDiv = () => {
   });
   steps[checker].classList.add("active");
   num[checker].classList.add("active");
-  checker++;
-  console.log(checker);
 };
 btn_step.addEventListener("click", () => {
   checkDiv();
+  // validate();
 });
 
-// Implementing the back feature
+// Implementing the Go back feature
 goBack = () => {
   if (checker == 0) {
   } else {
@@ -59,7 +74,6 @@ const check = () => {
     pricePeriod.forEach((div) => {
       div.classList.toggle("active");
     });
-    // pricePeriod[1].classList.
   } else {
     pricePeriod.forEach((div) => {
       div.classList.toggle("active");
@@ -67,8 +81,46 @@ const check = () => {
   }
 };
 slider.addEventListener("click", check);
-// Moving from step 2 to step 3
-// btn_2.addEventListener("click", () => {
-//   step_2.classList.remove("active");
-//   step_3.classList.add("active");
+
+// Implementing the pick add ons feature in step 3
+step_3_div.forEach((step_3_div) => {
+  step_3_div.addEventListener("click", (e) => {
+    step_3_div.classList.toggle("active");
+    active.forEach((div) => {
+      div.classList.remove("active");
+    });
+  });
+});
+if (steps[0].classList.contains("active")) {
+  backBtn.classList.add("none");
+}
+// Last Step
+window.addEventListener("click", () => {
+  if (steps[0].classList.contains("active")) {
+    backBtn.classList.add("none");
+  }
+  if (steps[0].classList.contains("active")) {
+  } else {
+    backBtn.classList.remove("none");
+  }
+  if (steps[3].classList.contains("active")) {
+    btn_step.textContent = "Confirm";
+  }
+  if (steps[4].classList.contains("active")) {
+    num[3].classList.add("active");
+    btn_step.classList.add("none");
+    backBtn.classList.add("none");
+  }
+});
+
+// btn_change.addEventListener("click", () => {
+//   steps.forEach((e) => {
+//     e.classList.remove("active");
+//   });
+//   num.forEach((e) => {
+//     e.classList.remove("active");
+//   });
+//   steps[2].classList.add("active");
+//   num[2].classList.add("active");
+//   console.log("few");
 // });
